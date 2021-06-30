59a771cd57fad71fa446719a3e483b2e65666956
faycal-dev <m.elmogherbi@esi-sba.dz>
2021-07-01 00:57:57 +0100

    second commit

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -9,7 +9,7 @@ const themeConfig = {
   navbarType: "floating", // options[String]: floating(default) / static / sticky / hidden
   footerType: "hidden", // options[String]: static(default) / sticky / hidden
   disableCustomizer: true, // options[Boolean]: true, false(default)
-  hideScrollToTop: false, // options[Boolean]: true, false(default)
+  hideScrollToTop: true, // options[Boolean]: true, false(default)
   disableThemeTour: false, // options[Boolean]: true, false(default)
   menuTheme: "primary", // options[String]: primary / success / danger / info / warning / dark
   direction: "ltr" // options[String] : ltr / rtl
