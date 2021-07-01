4f0d93fb79fc9f75b140d8bb1183241bfb025802
faycal-dev <m.elmogherbi@esi-sba.dz>
2021-07-01 02:27:52 +0100

    adding email logic

--- src/views/apps/calendar/AddEventButton.js
+++ src/views/apps/calendar/AddEventButton.js
@@ -1,27 +1,44 @@
-import React from "react"
-import { Button } from "reactstrap"
-import { Plus } from "react-feather"
-import { connect } from "react-redux"
+import React from "react";
+import { Button } from "reactstrap";
+import { Plus } from "react-feather";
+import { connect } from "react-redux";
+import emailjs from "emailjs-com";
 import {
   handleSidebar,
-  handleSelectedEvent
-} from "../../../redux/actions/calendar/index"
-const AddEventButton = props => {
+  handleSelectedEvent,
+} from "../../../redux/actions/calendar/index";
+const AddEventButton = (props) => {
   return (
     <Button.Ripple
       color="primary"
       onClick={() => {
-        props.handleSidebar(true)
-        props.handleSelectedEvent(null)
+        // props.handleSidebar(true)
+        // props.handleSelectedEvent(null)
+
+        emailjs
+          .send(
+            "service_h83tkke",
+            "template_pewhnzf",
+            { title: "hello", email: "test", message: "test test test" },
+            "user_xHUQf13pRDK3FK2uPVbGH"
+          )
+          .then(
+            (result) => {
+              console.log(result.text);
+            },
+            (error) => {
+              console.log(error.text);
+            }
+          );
       }}
       className="d-sm-block d-none"
     >
       {" "}
       <Plus size={15} /> <span className="align-middle">Add</span>
     </Button.Ripple>
-  )
-}
+  );
+};
 
 export default connect(null, { handleSidebar, handleSelectedEvent })(
   AddEventButton
-)
+);
